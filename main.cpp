1111158355f8d8af1f096629f58a174ee8643971
Adrian <a.kulawik8@gmail.com>
2021-02-16 17:33:07 +0100

    Deklaracja funkcji

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
+
+struct Pytanie
+{
+    int odpGracza;
+    int odpPoprawna;
+
+    string nazwaPytania;
+    string odpowiedz[3];
+};
+
+int punkty;
+int iloscPytan;
+Pytanie *pytania;
+
+void Wczytaj();
+void Powitanie();
+void ZadajPytania();
+void LiczPunkty();
+void PrzedstawWynik();
+void Posprzataj();
 
 int main()
 {
