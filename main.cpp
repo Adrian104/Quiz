557d2f4d6e9e35b4ea39cdf1702c8782b6547e4b
kalinkasowa <79195428+kalinkasowa@users.noreply.github.com>
2021-02-18 14:05:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,13 +18,26 @@ int iloscPytan;
 Pytanie *pytania;
 
 void Wczytaj();
-void Powitanie();
+void Powitanie(); 
+{
+    count<<"Witam w Quiz'ie!"<<endl;
+    count<<"Na ekranie wyswietla sie pytania, na ktore musisz odpowiedziec."<<endl;
+    count<<"Na kazde pytanie odpowiedz klikajac jedna z liter: "<<" 'A' lub 'B' lub 'C'."<<endl;
+    count<<"Na koncu dostaniesz informacje ile punktow zdobyles/as!"<<endl;
+    count<<"Powodzenia!"
+}
 void ZadajPytania();
 {
     cout << pytania[0].nazwaPytania << endl;
 }
 void LiczPunkty();
 void PrzedstawWynik();
+{
+    count<<"Gratulacje ukonczyles/as Quiz!"<<endl;
+    count<<"Twoj wynik to: "<<wynik<<"\n";
+    count<<"Brawo!"
+}
+    
 
 void Posprzataj()
 {
